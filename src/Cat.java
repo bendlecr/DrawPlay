8ebd68f248885aa2dfd2650e106efb934b7b2a88
crbendle <connorbendle10@gmail.com>
2023-02-14 16:20:10 -0700

    Connor changed text under cat from meow to hello

--- src/Cat.java
+++ src/Cat.java
@@ -44,6 +44,6 @@ public class Cat {
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Hello", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
